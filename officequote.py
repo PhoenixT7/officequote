7a86147820279ffffbd0cc26cd1b46211cccfc54
Mike Santiago <64493842+PhoenixT7@users.noreply.github.com>
2020-11-20 11:14:54 -0600

    Kode v1
    Unfinished, still need to write more quotes and finish the faulty random picker.

---
+++ officequote.py
@@ -0,0 +1,193 @@
+quote1 = "Sometimes I’ll start a sentence and I don’t even know where it’s going. I just hope I find it along the way. Michael Scott"
+quote2 = "And I feel God in this Chili’s tonight. – Pam Beesley"
+quote3 = "I DECLARE, BANKRUPTCY. - Michael Scott"
+quote4 = "That's What She Said. - Michael Scott, 23 Times"
+quote5 = "That's What She Said. - Pam Beesly, Once"
+quote6 = "That's What She Said. - Dwight Schrute, 4 Times"
+quote7 = "That's What She Said. - Jim Halpert, Twice"
+quote8 = "That's What She Said. - David Brent, Once"
+quote9 = "That's What She Said. - Darryl Philbin, Twice"
+quote10 = "That's What She Said. - Holly Flax, Once"
+quote11= "That's What She Said. - Jan Levinson,Once"
+quote12 = "That's What He Said. - Michael Scott, Twice"
+quote13 = "That's What He Said. - Oscar Martinez, Once"
+quote14 = "Eso es lo que dice, el! - Michael Scott, Once"
+quote15 = "The Dunder Mifflin stock symbol is D.M.I. Do you know what that stands for? Dummies, Morons, and Idiots. Because that’s what you’d have to be to own it. And as one of those idiots, I believe the board owes me answers. – Oscar Martinez"
+quote16 = "Me think, why waste time say lot word, when few word do trick. – Kevin Malone"
+quote17 = "I’m not superstitious, but I am a little stitious. – Michael Scott"
+quote18 = "I got six numbers, one more and it would have been a complete phone number. – Kevin Malone"
+quote19 = "Oh you’re paying way too much for worms. Who’s your worm guy? – Creed Bratton"
+quote20 = "I wonder what people like about me. Probably my jugs. – Phyllis Lapin-Vance"
+quote21 = "Today, smoking is going to save lives. – Dwight Schrute"
+quote22 = "Who is Justice Beaver? — Dwight Schrute"
+quote23 = "I am Beyonce, always. — Michael Scott"
+quote24 = "If I can’t scuba, then what’s this all been about? What am I working toward? – Creed Bratton"
+quote25 = "I love inside jokes. I’d love to be a part of one someday. — Michael Scott"
+quote26 = "Boy, have you done lost your mind? Cause I’ll help you find it! — Stanley Hudson"
+quote27 = "Jim told me you could buy gay-dar online. — Dwight"
+quote28 = "Nobody steals from Creed Bratton and gets away with it. The last person to do this disappeared. His name? Creed Bratton. – Creed Bratton"
+quote29 = "I already won the lottery. I was born in the US of A baby. And as backup I have a Swiss passport. – Creed Bratton"
+quote30 = "I want you to rub butter on my foot…Pam, please? I have Country Crock. — Michael Scott"
+quote31 = "Dwight you ignorant slut. — Michael Scott"
+quote32 = "You know a human can go on living for several hours after being decapitated. — Creed Bratton"
+quote33 = "When Pam gets Michael’s old chair, I get Pam’s old chair. Then I’ll have two chairs. Only one to go. — Creed Bratton"
+quote34 = "Creed Bratton has never declared bankruptcy. When Creed Bratton gets in trouble, he transfers his debt to William Charles Scheider. — Creed Bratton"
+quote35 = "I am a black belt in gift wrapping. – Jim Halpert"
+quote36 = "I am running away from my responsibilities. And it feels good. – Michael Scott"
+quote37 = "Do I need to be liked? Absolutely not. I like to be liked. I enjoy being liked. I have to be liked, but it’s not like this compulsive need to be liked, like my need to be praised. – Michael Scott"
+quote38 = ""
+quote39 = ""
+quote40 = ""
+quote41 = ""
+quote42 = ""
+quote43 = ""
+quote44 = ""
+quote45 = ""
+quote46 = ""
+quote47 = ""
+quote48 = ""
+quote49 = ""
+quote50 = ""
+quote51 = ""
+quote52 = ""
+quote53 = ""
+quote54 = ""
+quote55 = ""
+quote56 = ""
+quote57 = ""
+quote58 = ""
+quote59 = ""
+quote60 = ""
+quote61 = ""
+quote62 = ""
+quote63 = ""
+quote64 = ""
+quote65 = ""
+quote66 = ""
+quote67 = ""
+quote68 = ""
+quote69noice= "I want to be wine and dined and sixty nined. – Kevin Malone"
+quote70 = ""
+quote71 = ""
+quote72 = ""
+quote73 = ""
+quote74 = ""
+quote75 = ""
+quote76 = ""
+quote77 = ""
+quote78 = ""
+quote79 = ""
+quote80 = ""
+quote81 = ""
+quote82 = ""
+quote83 = ""
+quote84 = ""
+quote85 = ""
+quote86 = ""
+quote87 = ""
+quote88 = ""
+quote89 = ""
+quote90 = ""
+quote91 = ""
+quote92 = ""
+quote93 = ""
+quote94 = ""
+quote95 = ""
+quote96 = ""
+quote97 = ""
+quote98 = ""
+quote99 = ""
+quote100 = ""
+quote101 = ""
+quote102 = ""
+quote103 = ""
+quote104 = ""
+quote105 = ""
+quote106 = ""
+quote107 = ""
+quote108 = ""
+quote109 = ""
+quote110 = ""
+quote111 = ""
+quote112 = ""
+quote113 = ""
+quote114 = ""
+quote115 = ""
+quote116 = ""
+quote117 = ""
+quote118 = ""
+quote119 = ""
+quote120 = ""
+quote121 = ""
+quote122 = ""
+quote123 = ""
+quote124 = ""
+quote125 = ""
+quote126 = ""
+quote127 = ""
+quote128 = ""
+quote129 = ""
+quote130 = ""
+import random
+print (random.choice([quote38, quote39, quote40, quote41, quote42, quote43, quote44, quote45, quote46, quote47, quote48, quote49, quote50 ,quote51, quote52, quote53, quote54, quote55, quote56, quote57, quote58, quote59, quote60, quote61, quote62, quote63, quote64, quote65, quote66, quote67, quote68, quote69noice,]))
+quote70 
+quote71 
+quote72 
+quote73 
+quote74 
+quote75 
+quote76 
+quote77 
+quote78 
+quote79 
+quote80 
+quote81 
+quote82 
+quote83 
+quote84 
+quote85 
+quote86
+quote87 
+quote88 
+quote89 
+quote90 
+quote91 
+quote92 
+quote93 
+quote94 
+quote95 
+quote96 
+quote97 
+quote98 
+quote99 
+quote100 
+quote101 
+quote102 
+quote103 
+quote104 
+quote105 
+quote106 
+quote107 
+quote108 
+quote109 
+quote110 
+quote111 
+quote112 
+quote113 
+quote114 
+quote115 
+quote116 
+quote117 
+quote118 
+quote119 
+quote120 
+quote121 
+quote122 
+quote123 
+quote124 
+quote125 
+quote126 
+quote127 
+quote128 
+quote129 
+quote130
